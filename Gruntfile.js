2ceda0d119b1aaa6ea25d9767a42946f1424dc13
Jose Canahui <fonderjose@Joses-MBP.home>
2015-07-15 14:06:04 -0400

    added languages path

--- Gruntfile.js
+++ Gruntfile.js
@@ -361,7 +361,8 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
-            'index.html'
+            'index.html',
+            'languages/**/*'
           ]
         }, {
           expand: true,
